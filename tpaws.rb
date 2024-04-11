cdfa97433c9f584335529b4e8763ce7aa3bd5a03
Federico Vitale <federico.vitale@satispay.com>
2024-04-11 12:10:41 +0200

    tpaws: updated version

--- tpaws.rb
+++ tpaws.rb
@@ -1,11 +1,11 @@
 class Tpaws < Formula
-  desc "CLI to manage TargetProcess and AWS CodeCommit"
-  homepage "https://github.com/rawnly/tpaws"
-  url "https://github.com/rawnly/tpaws/releases/download/0.0.1/tpaws.tar.gz"
-  sha256 "8038ae79d826ba9c7b4a7089fb93ff8ed6105d8eded061cf69ccb932f3e19430"
-  version "0.0.1"
+  desc 'CLI to manage TargetProcess and AWS CodeCommit'
+  homepage 'https://github.com/rawnly/tpaws'
+  url 'https://github.com/rawnly/tpaws/releases/download/0.0.2/tpaws.tar.gz'
+  sha256 '061a029e6c6845de477fa756ffbcd9d6dcc60796dd0bb10bb3c5bcb58ab637c4'
+  version '0.0.2'
 
   def install
-    bin.install("tpaws")
+    bin.install 'tpaws'
   end
 end
